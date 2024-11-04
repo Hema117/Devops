38006b860ba720cad85724b036d25237017e3394
Hema117 <96809061+Hema117@users.noreply.github.com>
2024-11-04 20:07:56 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -1,12 +1,4 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
-resource "aws_instance" "example" {
+resource "aws_instance" "my_instance" {
   ami           = "ami-0dee22c13ea7a9a67"
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "MyInstance"
-  }
 }
